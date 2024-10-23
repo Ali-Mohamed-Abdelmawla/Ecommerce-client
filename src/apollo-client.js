489fd81781f18b9a1f91609fb3bf397d141bd7f2
Ali-Mohamed-Abdelmawla <alizeyad407@gmail.com>
2024-10-23 20:33:19 +0300

    deployment with protection

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,13 +1,30 @@
 // src/apollo-client.js
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+// Your ngrok URI and authentication details
+const username = 'your-username';
+const password = 'your-password';
+const encodedCredentials = btoa(`${username}:${password}`);
+
+// Create HTTP Link to the GraphQL endpoint
 const httpLink = createHttpLink({
-  // uri: 'http://localhost/ecommerce2/public/index.php', // Replace with your actual GraphQL endpoint
   uri: 'https://828c-197-36-122-196.ngrok-free.app/ecommerce2/public/index.php'
 });
 
+// Set the Authorization header for Basic Authentication
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      Authorization: `Basic ${encodedCredentials}`
+    }
+  };
+});
+
+// Combine authLink and httpLink
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
