2f3eb8c13e28df06a8360b011f0a5d4eef4e28de
Ali-Mohamed-Abdelmawla <alizeyad407@gmail.com>
2024-11-08 21:32:44 +0200

    endpoint

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
   // uri: 'http://localhost/ecommerce/public/index.php', // Replace with the deployment GraphQL endpoint
-  uri: "https://5da2-35-180-79-2.ngrok-free.app/ecommerce/public/index.php",
+  uri: "https://d367-197-36-230-4.ngrok-free.app/ecommerce/public/index.php",
 });
 
 const client = new ApolloClient({
